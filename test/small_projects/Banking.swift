1ad8d265ef48b0c9bdf45a343f1f0c45f3cf79ee
EPowers25 <egpowers25@gmail.com>
2025-10-02 23:45:47 -0400

    Banking is working

--- test/small_projects/Banking.swift
+++ test/small_projects/Banking.swift
@@ -19,19 +19,22 @@ while stop == 0 {
             let attempt = readLine() ?? ""
             if attempt == password{
                 print("How much would you like to depsoit:$ ")
-                balance = balance + Double(readLine() ?? "0.0")!
-                print("You have successfully deposited", Double(readLine()?? 0.0), "to your account. Your new balance is:$", balance)
+                var amount = Double(readLine() ?? "0.0") ?? 0.0
+                balance = balance + amount
+                print("You have successfully deposited",amount,"to your account. Your new balance is:$",balance)
             }
         case 2:
         print("Enter password: ")
             let attempt = readLine() ?? ""
             if attempt == password {
                 print("How much would you like to withdraw: $")
-                if balance == balance - Double(readLine() ?? "0.0")! > 0.00{
-                    balance = balance - Double(readLine() ?? "0.0")!
-                    print("You have successfully withdrawn", Double(readLine() ?? 0.0)!, "from your account. Your new balancec is:$", balance)
-                } else {
-                    print("Insufficent funds")
+                if let amount = Double(readLine() ?? "0.0"){
+                    if balance - amount > 0.0{
+                        balance -= amount
+                        print("You have successfully withdrawn",amount, "from your account. Your new balancec is:$",balance)
+                    } else {
+                        print("Insufficent funds")
+                    }
                 }
             }
         case 3:
