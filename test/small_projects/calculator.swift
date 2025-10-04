2d0964c900c5eeba244fc59d6be284ffeedb01e6
EPowers25 <egpowers25@gmail.com>
2025-10-03 23:56:06 -0400

    Expanding some small projects

--- test/small_projects/calculator.swift
+++ test/small_projects/calculator.swift
@@ -1,12 +1,51 @@
 import Foundation
-//Calculator for two numbers
-
-print("Enter your first number: ", terminator: "")
-let numb1 = Double(readLine() ?? "0") ?? 0
-print("Enter your second number: ", terminator: "")
-let numb2 = Double(readLine() ?? "0") ?? 0
+//Calculator for two numbers WIP
+var proceed = 0
+var numb1 = 0.0
+var numb2 = 0.0
+var operation = ""
+func transform(from ASCIIValue: Int) -> Double {
+    return Double(UnicodeScalar(ASCIIValue)!)
+}   
+while proceed == 0{
+    print("Enter your first number: ", terminator: "")
+    numb1 = Double(readLine() ?? "0")!
+    numb1 = transform(numb1)
+    if numb1 < 47{
+        print("Incorrect syntax")
+    }
+    else if numb1 > 57{
+        print("Incorrect syntax")
+    } else {
+        proceed += 1
+    }
+}
+while proceed == 1{
+    print("Enter your second number: ", terminator: "")
+    numb2 = Double(readLine() ?? "0")!
+    numb2 = transform(numb1)
+    if numb2 < 47{
+        print("Incorrect syntax")
+    }
+    else if numb2 > 57{
+        print("Incorrect syntax")
+    } else {
+        proceed += 1
+    }
+}
+while proceed == 2{
+    print("Enter your first number: ", terminator: "")
+    numb1 = Double(readLine() ?? "0")!
+    numb1 = transform(numb1)
+    let allowed: [UInt8] = [42, 43, 45, 47, 94]
+    if let value = numb1, !allowed.contains(value){
+        print("Incorrect syntax")
+    } else {
+        proceed += 1
+    }
+}
 print("What operation do you want (+, /, -, *, ^): ")
-let operation = readLine() ?? "/"
+operation = readLine() ?? ""
 var result: Double = 0
 
 switch operation{
