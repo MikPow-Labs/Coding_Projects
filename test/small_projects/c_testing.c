749d1a2ae5e6f94b8c14214c279116bdddd273aa
EPowers25 <egpowers25@gmail.com>
2025-10-24 07:51:33 -0400

    Starting to try out C

---
+++ test/small_projects/c_testing.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+/*
+int main() {
+    char color[20];
+    char pluralNoun[20];
+    char celebrityF[20];
+    char celebrityL[20];
+
+    printf("Enter a color: ");
+    scanf("%s", color);
+    printf("Enter a plural noun: ");
+    scanf("%s", pluralNoun);
+    printf("Enter a celebrity: ");
+    scanf("%s%s", celebrityF, celebrityL);
+
+    printf("Roses are %s\n", color);
+    printf("%s are blue\n", pluralNoun);
+    printf("I love %s %s\n", celebrityF, celebrityL);
+
+    return(0); 
+}
+*/
+
+int main() {
+    int luckyNumbers[] = {4, 7, 8, 16, 24};
+
+    return(0);
+}
