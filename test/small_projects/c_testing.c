dca043c1421b8db10efb3e1ed9ac51f2101f6d2a
EPowers25 <egpowers25@gmail.com>
2025-11-06 12:56:16 -0500

    More C

--- test/small_projects/c_testing.c
+++ test/small_projects/c_testing.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-/*
+#include <string.h>
+
 int main() {
     char color[20];
     char pluralNoun[20];
@@ -9,22 +10,32 @@ int main() {
     char celebrityL[20];
 
     printf("Enter a color: ");
-    scanf("%s", color);
+    fgets(color, 20, stdin);
+    color[strcspn(color, "\n")] = 0;
+
     printf("Enter a plural noun: ");
-    scanf("%s", pluralNoun);
-    printf("Enter a celebrity: ");
-    scanf("%s%s", celebrityF, celebrityL);
+    fgets(pluralNoun, 20, stdin);
+    pluralNoun[strcspn(pluralNoun, "\n")] = 0;
+
+    printf("Enter a celebrity first name: ");
+    fgets(celebrityF, 20, stdin);
+    celebrityF[strcspn(celebrityF, "\n")] = 0;
+
+    printf("Enter a celebrity last name: ");
+    fgets(celebrityL, 20, stdin);
+    celebrityL[strcspn(celebrityL, "\n")] = 0;
 
     printf("Roses are %s\n", color);
     printf("%s are blue\n", pluralNoun);
     printf("I love %s %s\n", celebrityF, celebrityL);
 
-    return(0); 
+    return 0;
 }
-*/
-
+/*
 int main() {
     int luckyNumbers[] = {4, 7, 8, 16, 24};
-
+    luckyNumbers[2] = 200;
+    printf("%d\n", luckyNumbers[2]);
     return(0);
 }
+*/
