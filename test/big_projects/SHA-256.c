1db96313954e0c2eadec61503696c60f639fb384
EPowers25 <egpowers25@gmail.com>
2025-11-16 03:24:50 -0500

    Added in padding for message

--- test/big_projects/SHA-256.c
+++ test/big_projects/SHA-256.c
@@ -2,11 +2,24 @@
 #include <string.h>
 
 int* encrypt(char message[1000]){
+    message[strcspn(message, "\n")] = '\0';
     int length = strlen(message);
     static int characters[2000];
-    for (int i = 0; i < length; i ++){
+    int i;
+    unsigned long long bitLength = length * 8;
+    for (i = 0; i < length; i ++){
         characters[i] = (unsigned char) message[i];
     }
+    characters[i] = 0x80;
+    i += 1;
+    while (i%64 < 56){
+        characters[i++] = 0x00;
+    }
+    for (int j = 0; j < 8; j++) {
+    characters[i + j] = (bitLength >> (8 * (7 - j))) & 0xFF;
+    }
+    i += 8; 
+    characters[i] = -1;
     return characters;
 }
 
@@ -15,8 +28,8 @@ int main(){
     printf("What is the message you want you encrypt?\n");
     fgets(message, sizeof(message), stdin);
     int* encrypted = encrypt(message);
-    for (int i = 0; encrypted[i] != 10; i++){
-        printf("%d", encrypted[i]);
+    for (int i = 0; encrypted[i] != -1; i++){
+        printf("0x%02X ", encrypted[i]);
     }
     printf("\n");
     return(0);
