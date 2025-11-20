cce87e78094be8cf7b25d6f0d19f3dbef05827c2
TseldoraCap <drmik@DRM.localdomain>
2025-11-19 20:01:35 -0500

    reversing ig work

---
+++ test/small_projects/reversing.py
@@ -0,0 +1,46 @@
+def transformFlag(flag):
+    
+    res = ""
+    
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    digits = "0123456789"
+    special = "!@#$%^&*()_+{}[]|"
+    
+    for i in range(len(flag)):
+        char = flag[i]
+        
+        # Check alphabet
+        if char in alphabet:
+            c = alphabet.index(char)
+            ind = c + i
+            res += alphabet[ind % len(alphabet)]
+        
+        # Check digits
+        elif char in digits:
+            c = digits.index(char)
+            ind = (i * 2) + c
+            res += digits[ind % len(digits)]
+        
+        # Check special characters
+        elif char in special:
+            c = special.index(char)
+            ind = (i * i) + c
+            res += special[ind % len(special)]
+    
+    return res
+
+"""
+def reverseFlag(transformed):
+    length = len(transformed)
+    reversed = ""
+    for i in range(length):
+"""
+    
+
+def main():
+    tranFlag = transformFlag("flag{m0d1fy!}")
+    print(tranFlag)
+    #print(reverseFlag(tranFlag))
+    #print(reverseFlag("idvi+1{s6e3{)arg2zv[moqa905+"))
+
+main()
