4e815aabc3c35a217abb914cb2e097bbc4dceab1
TseldoraCap <drmik@DRM.localdomain>
2025-11-20 15:57:52 -0500

    Finished reversing the flag from yesterday

--- test/small_projects/reversing.py
+++ test/small_projects/reversing.py
@@ -1,5 +1,7 @@
 def transformFlag(flag):
-    
+    """
+    Original Kotlin function but remade in python
+    """
     res = ""
     
     alphabet = "abcdefghijklmnopqrstuvwxyz"
@@ -29,18 +31,33 @@ def transformFlag(flag):
     
     return res
 
-"""
+
 def reverseFlag(transformed):
     length = len(transformed)
     reversed = ""
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    digits = "0123456789"
+    special = "!@#$%^&*()_+{}[]|"
     for i in range(length):
-"""
-    
+        charIndex = transformed[i]
+        if charIndex in alphabet:
+            c = alphabet.index(charIndex)
+            ind = c - i
+            reversed += alphabet[ind % len(alphabet)]
+        elif charIndex in digits:
+            c = digits.index(charIndex)
+            ind = c - int(i * 2)
+            reversed += digits[ind % len(digits)]
+        elif charIndex in special:
+            c = special.index(charIndex)
+            ind = c - (i * i)
+            reversed += special[ind % len(special)]
+    return reversed
 
 def main():
-    tranFlag = transformFlag("flag{m0d1fy!}")
-    print(tranFlag)
+    #tranFlag = transformFlag("flag{m0d1fy!}")
+    #print(tranFlag)
     #print(reverseFlag(tranFlag))
-    #print(reverseFlag("idvi+1{s6e3{)arg2zv[moqa905+"))
+    print(reverseFlag("idvi+1{s6e3{)arg2zv[moqa905+"))
 
 main()
